b458c8fad993e97261bffc3392340ee9ec54a835
arielr97 <arielr.z97@outlook.com>
2026-06-03 16:03:04 -0300

    Agregando imagenes para prueba y cambios para poder visualizarlas

--- public/js/productos.js
+++ public/js/productos.js
@@ -1,3 +1,15 @@
+function crearTarjetaProducto(producto) {
+    return `
+        <div class="producto">
+            <h3>${producto.nombre}</h3>
+            <p>Tipo: ${producto.tipo}</p>
+            <img src="${producto.img}" alt="${producto.nombre}">
+            <p>Precio: $${producto.precio}</p>
+            <button type="button" class="btn-guardar">Agregar al carrito</button>
+        </div>
+    `;
+}
+
 async function traerProductos() {
     try{
         const respuesta = await fetch("/api/productos");
@@ -5,8 +17,11 @@ async function traerProductos() {
         if(!respuesta.ok){
             throw new Error("Error al obtener productos");
         }else{
-            const data = await respuesta.json();
-            console.log(data);
+            const productos = await respuesta.json();
+            productos.forEach(producto => {
+                const contenedor = document.getElementById("contenedor-productos");
+                contenedor.innerHTML += crearTarjetaProducto(producto);
+            });
         }
     }catch(e){
         console.error(e);
