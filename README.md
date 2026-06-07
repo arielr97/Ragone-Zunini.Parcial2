fb384eb9d08a5e56d78b64df76078048522d960a
Matiaz31 <matiaszunini31@gmail.com>
2026-06-07 18:14:09 -0300

    pruba base de datos

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Ragone-Zunini.Parcial2
 
+## Credenciales de administrador
+
+**Email:** `ola@mail.asd`  
+**Contraseña:** `admin123`
+> Estas credenciales están destinadas únicamente para pruebas y evaluación de la aplicación.
+
 
--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
+const bcrypt = require("bcrypt");
 
 const sequelize = require("./config/database");
 
-require("./models");
+const { Admin, Producto, Venta } = require("./models");
 
 app.use(express.static("public"));
 
@@ -12,8 +13,18 @@ const routerProductos = require("./routes/RouterProductos");
 app.use("/api/productos", routerProductos);
 
 sequelize.sync()
-    .then(() => {
+    .then(async () => {
         console.log("Base creada");
+        
+        const admin = await Admin.findOne({ where: { email: "ola@mail.asd" } });
+        if (!admin) {
+            const hash = await bcrypt.hash("admin123", 10);
+            console.log(hash);
+            await Admin.create({
+                email: "ola@mail.asd",
+                password: hash
+            });
+        }
 
         app.listen(3000, () => {
             console.log("Servidor iniciado");
@@ -21,4 +32,4 @@ sequelize.sync()
     })
     .catch((error) => {
         console.error("Error al conectar la BD:", error);
-    });
+});
