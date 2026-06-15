80743720fc838abb03626fa648e3bf11be60ba6e
arielr97 <arielr.z97@outlook.com>
2026-06-14 22:05:14 -0300

    Vista carrito mejorada

--- public/js/carrito.js
+++ public/js/carrito.js
@@ -1,4 +1,13 @@
 let contenedorCarrito = document.getElementById("contenedor-carrito");
+let botonFinalizarCompra = document.getElementById("finalizar-compra");
+let botonVaciarCarrito = document.getElementById("vaciar-carrito");
+let contenedorTotal = document.getElementById("contenedor-total");
+
+function contarElementoReduce(array, elementoBuscado) {
+    return array.reduce((contador, elemento) => {
+        return elemento.nombre === elementoBuscado.nombre ? contador + 1 : contador;
+    }, 0);
+}
 
 function crearTarjetaCarrito(producto){
     const tarjeta = document.createElement("div");
@@ -10,6 +19,9 @@ function crearTarjetaCarrito(producto){
     img.alt = producto.nombre;
     const precio = document.createElement("p");
     precio.textContent = `Precio: $${producto.precio}`;
+    const cantidad = document.createElement("p");
+    let listaCarrito = JSON.parse(localStorage.getItem("carrito"));
+    cantidad.textContent = `Cantidad: ${contarElementoReduce(listaCarrito, producto)}`
     const boton = document.createElement("button");
     boton.textContent = "Quitar del carrito";
     boton.addEventListener("click", () => {
@@ -19,6 +31,7 @@ function crearTarjetaCarrito(producto){
     tarjeta.appendChild(titulo);
     tarjeta.appendChild(img);
     tarjeta.appendChild(precio);
+    tarjeta.appendChild(cantidad);
     tarjeta.appendChild(boton);
 
     return tarjeta;
@@ -29,7 +42,7 @@ function quitarDelCarrito(producto){
     const indice = carrito.findIndex(elem => elem.id === producto.id);
     console.log("indice: " + indice);
     if (indice !== -1) {
-    carrito.splice(indice, 1);
+        carrito.splice(indice, 1);
     }
     localStorage.setItem("carrito", JSON.stringify(carrito));
     dibujarCarrito();
@@ -37,12 +50,28 @@ function quitarDelCarrito(producto){
 
 function dibujarCarrito(){
     contenedorCarrito.innerHTML = "";
-
+    contenedorTotal.innerHTML = "";
     let listaCarrito = JSON.parse(localStorage.getItem("carrito"));
-
-    listaCarrito.forEach(producto => {
+    let listaSinDuplicados = listaCarrito.filter((objeto, indice, array) => {
+        return array.findIndex(item => item.nombre === objeto.nombre) === indice;
+    });
+    listaSinDuplicados.forEach(producto => {
     contenedorCarrito.appendChild(crearTarjetaCarrito(producto));
     });
+
+    const totalCarrito = listaCarrito.reduce(
+        (acum, producto) =>
+        acum + producto.precio, 0
+    );
+    const total = document.createElement("p");
+    total.textContent = `Total: $${totalCarrito}`;
+    contenedorTotal.appendChild(total);
 }
+
+botonVaciarCarrito.addEventListener("click", () => {
+    let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+    localStorage.setItem("carrito", JSON.stringify([]));
+    dibujarCarrito();
+})
 
 dibujarCarrito();
