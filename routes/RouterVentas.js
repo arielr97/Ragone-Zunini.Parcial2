1b5923213ea78c3835a85fc20ea53a7a04e5eac3
Matiaz31 <matiaszunini31@gmail.com>
2026-06-17 18:48:33 -0300

    Editar imagen de productos

--- routes/RouterVentas.js
+++ routes/RouterVentas.js
@@ -4,6 +4,11 @@ const routerVentas = express.Router();
 
 const {crearVenta, obtenerVentas, obtenerVentaPorId} = require("../controllers/ventaController");
 
+routerVentas.use((req, res, next) => {
+    console.log("🔥 Entró a RouterVentas:", req.method, req.url);
+    next();
+});
+
 routerVentas.post("/", crearVenta);
 
 routerVentas.get("/", obtenerVentas);
@@ -11,3 +16,5 @@ routerVentas.get("/", obtenerVentas);
 routerVentas.get("/:id", obtenerVentaPorId);
 
 module.exports = routerVentas;
+
+console.log("✅ RouterVentas cargado");
